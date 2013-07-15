30db8e0e618b79e3c99cf59c59d0bde9d85abfe6
John Murphy <john.mmurphy@gmail.com>
2013-07-15 12:08:40 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,13 +1,14 @@
-var express = require('express');
+var http = require('http');
+var fs = require('fs');
+var sys = require('sys');
 
-var app = express.createServer(express.logger());
-
-var fs = require("fs");
-
-fs.readFile(index.html", function (err, data) {
-    if (err) throw err;
-    console.log(data.toString());
-});
+http.createServer(function(req, res){
+    fs.readFile('index.html',function (err, data){
+        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
+        res.write(data);
+        res.end();
+    });
+}).listen(8000);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
