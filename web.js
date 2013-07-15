d5547f2f69f29f8300398e388d2d07db190cc3f0
John Murphy <john.mmurphy@gmail.com>
2013-07-15 12:01:47 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World2!');
+var fs = require("fs");
+
+fs.readFile(index.html", function (err, data) {
+    if (err) throw err;
+    console.log(data.toString());
 });
 
 var port = process.env.PORT || 5000;
